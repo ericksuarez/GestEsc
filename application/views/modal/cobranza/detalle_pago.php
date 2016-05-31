836d1ca7aab6967745659f65e44942ba931a8c62
esuarez <esuarez@carloslap.rocasis.com>
2016-05-31 12:52:41 -0500

    Modal para visualizar los detalles.Modulo de detalles de Pago visualizacion

---
+++ application/views/modal/cobranza/detalle_pago.php
@@ -0,0 +1,51 @@
+<div class="modal slide-down fade" id="detalle-pago">
+    <div class="modal-dialog">
+        <div class="v-cell">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title">Detalle de Pago</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="panel panel-default">
+                        <div id="resp-FP" style="z-index: 100;position: fixed;"></div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <table class="table" id="lista_detalles">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <!--<th>Fec.Pago</th>-->
+                                            <th>Unidad</th>
+                                            <th>Nombre</th>
+                                            <th>Descuento</th>
+                                            <th>Recargo</th>
+                                            <th>Subtotal</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td id="No"></td>
+                                            <!--<td id="FecPago"></td>-->
+                                            <td id="Unidad"></td>
+                                            <td id="Nombre"></td>
+                                            <td id="Descuento"></td>
+                                            <td id="Recargo"></td>
+                                            <td id="SubTotal"></td>
+                                            <td id="Total"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" id="FP-IDExp" value="">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal" id="DPcancelar">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
