e83f323a186754c10c34471c0bb1f19d17a72ca4
esuarez <esuarez@carloslap.rocasis.com>
2016-05-31 10:07:25 -0500

    Creacion del modulo de Cobranza pagos se crearon las funciones de forma de pago de la colegiatura

--- application/models/Pago_model.php
+++ application/models/Pago_model.php
@@ -1,46 +1,72 @@
 <?php
+
 /**
  * Description of Pago_model
  *
  * @author esuarez
  */
 class Pago_model extends CI_Model {
-    
+
     var $IDPago;
-    
-     function __construct() {
+
+    function __construct() {
         parent::__construct();
     }
-    
-    public function InsPago($post,$IDExp) {
+
+    public function InsPago($post, $IDExp) {
         $data = array(
             "Expediente_IDExp" => $IDExp,
-            "Descuento" => IsNotDefault($post,"Descuento"),
-            "Recargo" => IsNotDefault($post,"Recargo"),
-            "SubTotal" => IsNotDefault($post,"SubTotal"),
-            "Total" => IsNotDefault($post,"Total")
+            "Descuento" => IsNotDefault($post, "Descuento"),
+            "Recargo" => IsNotDefault($post, "Recargo"),
+            "SubTotal" => IsNotDefault($post, "SubTotal"),
+            "Total" => IsNotDefault($post, "Total")
         );
         $this->db->insert('pago', ToCleanData($data));
         $IDExpe = $this->db->insert_id();
         $this->setIDPago($IDExpe);
     }
-    
-    public function InsPagoDetalle($post,$IDExp,$IDServicio,$IDUnidad) {
+
+    public function InsPagoDetalle($post, $IDExp, $IDServicio, $IDUnidad) {
         $data = array(
             "Pago_IDPago" => $this->getIDPago(),
             "Expediente_IDExp" => $IDExp,
             "Servicio_IDServicio" => $IDServicio,
             "Unidad_IDUnidad" => $IDUnidad,
-            "Descuento" => IsNotDefault($post,"Descuento"),
-            "Recargo" => IsNotDefault($post,"Recargo"),
-            "SubTotal" => IsNotDefault($post,"SubTotal"),
-            "Total" => IsNotDefault($post,"Total")
+            "Descuento" => IsNotDefault($post, "Descuento"),
+            "Recargo" => IsNotDefault($post, "Recargo"),
+            "SubTotal" => IsNotDefault($post, "SubTotal"),
+            "Total" => IsNotDefault($post, "Total")
         );
         $this->db->insert('pago_has_servicio', ToCleanData($data));
         $IDExpe = $this->db->insert_id();
         return $IDExpe;
     }
+
+    public function InsFormaPago($post) {
+        $data = array(
+            "IDCuenta" => IsNotDefault($post,"Cuenta"),
+            "IDEstudiante" => IsNotDefault($post,"IDEstudiante"),
+            "IDPadFam" => IsNotDefault($post,"IDTutor"),
+            "FrecPago" => IsNotDefault($post, "FrecPago"),
+            "Dia" => IsNotDefault($post, "Dia")
+        );
+        $this->db->insert('cuenta', ToCleanData($data));
+        $cuenta = $this->db->insert_id();
+        return $cuenta;
+    }
     
+    public function UpdFormaPago($post) {
+        $data = array(
+            "FrecPago" => IsNotDefault($post, "FrecPago"),
+            "Dia" => IsNotDefault($post, "Dia")
+        );
+        $this->db->update('cuenta', $data, array(
+            "IDCuenta" => $post["Cuenta"],
+            "IDEstudiante" => $post["IDEstudiante"],
+            "IDPadFam" => $post["IDTutor"]
+        ));
+    }
+
     function getIDPago() {
         return $this->IDPago;
     }
@@ -49,7 +75,7 @@ class Pago_model extends CI_Model {
         $this->IDPago = $IDPago;
     }
 
-     public function getQuery($sql) {
+    public function getQuery($sql) {
         $query = $this->db->query($sql);
 
         if ($query->num_rows() > 0) {
@@ -70,4 +96,5 @@ class Pago_model extends CI_Model {
         }
         return $array[0];
     }
+
 }
