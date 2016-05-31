836e9cdd8c80b7ea7604fa72b13eb7e7cd9fefe7
esuarez <esuarez@carloslap.rocasis.com>
2016-05-31 16:48:06 -0500

    view de calificaciones del estudiante

--- application/views/estudiante/calificaciones.php
+++ application/views/estudiante/calificaciones.php
@@ -1,9 +1,10 @@
 <div id="content">
     <div class="container-fluid">
         <!--<div class="page-section">-->
-            <div class="row">
-                <div class="col-xs-12 col-md-12 col-lg-8 col-md-offset-1 col-lg-offset-2">
-                    <h4 class="page-section-heading">
+        <div class="row">
+
+            <div class="col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2">
+                <h4 class="page-section-heading">
                     <img src="<?= base_url(); ?>images/logo.jpg" width="100" alt="Logo" class="img-responsive pull-left" />
                     <p><label><?php echo $this->config->item('NomEscuela'); ?></label></p>
                     <p>
@@ -12,14 +13,14 @@
                 </h4>	
 
                 <div class="pull-right">
-                    <a class="btnPrint btn btn-primary " href='<?php echo site_url("exportar/impreso_horario_individual");  ?>' id="print">
+                    <a class="btnPrint btn btn-primary " href='<?php echo site_url("exportar/impreso_horario_individual"); ?>' id="print">
                         <i class="fa fa-file-pdf-o floatL t3"></i>
                         <span class="hidden-xs floatL l5">
                             <b>&nbsp; PDF</b>
                         </span>
                         <div class="clear"></div>
                     </a>
-                    <a class="btnPrint btn btn-primary " href='<?php echo site_url("exportar/impreso_horario_individual");  ?>' id="print">
+                    <a class="btnPrint btn btn-primary " href='<?php echo site_url("exportar/impreso_horario_individual"); ?>' id="print">
                         <i class="fa fa-print floatL t3"></i>
                         <span class="hidden-xs floatL l5">
                             <b>&nbsp; Imprimir</b>
@@ -28,117 +29,38 @@
                     </a>
                 </div>
 
-                    <div class="panel-heading panel-heading-white">
-                        <br><br>
-                        <p><label>Matricula: </label>&nbsp; <?php echo $estudiante[0]['Matricula']?></p>
-                        <p><label>Nombre: </label>&nbsp; <?php echo $estudiante[0]['NomCompleto']?></p>
-                    </div>
+                <div class="panel-heading panel-heading-white">
+                    <br><br>
+                    <p><label>Matricula: </label>&nbsp; <?php echo $estudiante[0]['Matricula'] ?></p>
+                    <p><label>Nombre: </label>&nbsp; <?php echo $estudiante[0]['NomCompleto'] ?></p>
+                </div>
 
-
-                    <div class="table-responsive">
-
+                <div class="table-responsive">
                         <!-- Pricing table -->
                         <table class="table panel panel-default table-pricing">
-
                             <!-- Table heading -->
                             <thead>
                                 <tr>
+                                    <th>Periodo</th>
                                     <th>Materia</th>
-                                    <th>1er Bimestre</th>
-                                    <th>2do Bimestre</th>
-                                    <th>3ro Bimestre</th>
-                                    <th>4to Bimestre</th>
-                                    <th>5to Bimestre</th>
-                                    <th>Extraordinario</th>
-                                    <th>Promedio</th>
+                                    <th>Tarea</th>
+                                    <th>Calificación</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php foreach ($calificaciones as $key => $value) {?>
                                 <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
+                                    <td><?php echo $value['Descripcion']?></td>
+                                    <td><?php echo $value['Nombre']?></td>
+                                    <td><?php echo $value['NomTarea']?></td>
+                                    <td><?php echo $value['Calificacion']?></td>
                                 </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
-                                <tr>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                    <td> --- </td>
-                                </tr>
+                                <?php }?>
                             </tbody>
                         </table>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 <!--</div>-->
