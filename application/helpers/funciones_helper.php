49b1716fa38711c6d3e79edc8421002862ddc5c0
esuarez <esuarez@carloslap.rocasis.com>
2016-05-31 10:06:30 -0500

    Creacion del modulo de Cobranza pagos funciones de fecha y pago de servicios

--- application/helpers/funciones_helper.php
+++ application/helpers/funciones_helper.php
@@ -30,7 +30,7 @@ function getCatOpciones($CatNombre, $NomSelect, $valor = "") {
     return $html;
 }
 
-function getCatOpcionesWhere($CatNombre, $NomSelect, $valor = "",$where="") {
+function getCatOpcionesWhere($CatNombre, $NomSelect, $valor = "", $where = "") {
     $CI = & get_instance();
     $CI->load->model("Catalogo_model", "catalogo");
 
@@ -44,7 +44,7 @@ function getCatOpcionesWhere($CatNombre, $NomSelect, $valor = "",$where="") {
     return $html;
 }
 
-function getCatOpcionesWhereText($CatNombre, $NomSelect, $valor = "",$where="") {
+function getCatOpcionesWhereText($CatNombre, $NomSelect, $valor = "", $where = "") {
     $CI = & get_instance();
     $CI->load->model("Catalogo_model", "catalogo");
 
@@ -52,7 +52,7 @@ function getCatOpcionesWhereText($CatNombre, $NomSelect, $valor = "",$where="") 
     $html = '';
     foreach ($data as $key => $value) {
         $tmp = $valor == $value['index'] ? $value['opcion'] : '';
-        $html .= '<p class="text"><b>'.$tmp. '</b></p>';
+        $html .= '<p class="text"><b>' . $tmp . '</b></p>';
     }
 
     return $html;
@@ -103,7 +103,7 @@ function getCatOpcionesCheckBoxes($CatNombre, $NomSelect, $valor = "") {
                             name="' . $NomSelect . '" id="1" data-price="' . $data[0]['Precio'] . '">
                           ' . $data[0]['opcion'] . ' 
                     </td>
-                    <td class="col-md-6"><i class="fa fa-fw fa-dollar"></i><strong>' . $data[0]['Precio'] . '</strong></td>
+                    <td class="col-md-6"><i class="fa fa-fw fa-dollar"></i><strong id="PriceText-1">' . $data[0]['Precio'] . '</strong></td>
                 </tr>';
     foreach ($data as $key => $value) {
         if ($value['index'] > 1) {
@@ -113,7 +113,7 @@ function getCatOpcionesCheckBoxes($CatNombre, $NomSelect, $valor = "") {
             $html .= '<input type="checkbox" value="' . $value['index'] . '" data-price="' . $value['Precio'] . '"'
                     . 'name="' . $NomSelect . '" id="' . $value['index'] . '"' . set_select($NomSelect, $value['index'], $tmp) . '>'
                     . ' ' . $value['opcion'] . ' </td>'
-                    . '<td class="col-md-6"><i class="fa fa-fw fa-dollar"></i><strong>' . $value['Precio'] . '</strong></td>';
+                    . '<td class="col-md-6"><i class="fa fa-fw fa-dollar"></i><strong id="PriceText-' . $value['index'] . '">' . $value['Precio'] . '</strong></td>';
             $html .= '</td></tr>';
         }
     }
@@ -187,7 +187,7 @@ function getCatOpcionesCheckBoxesWhere($CatNombre, $NomSelect, $valor = "", $whe
                 . '<i class="fa fa-trash-o"></i></button>'
                 . '<input type="checkbox" value="' . $value['index'] . '"'
                 . 'name="' . $NomSelect . '" id="' . $value['index'] . '"' . set_select($NomSelect, $value['index'], $tmp) . '>'
-                . '<label for="' . $value['index'] . '">&nbsp; ' . $value['opcion'] . '</label>';
+                . '<label id="PriceText-' . $value['index'] . '" for="' . $value['index'] . '">&nbsp; ' . $value['opcion'] . '</label>';
         $html .= '</div>';
     }
     return $html;
@@ -265,18 +265,24 @@ function regresa_URL() {
     return base_url() . 'index.php/' . $CI->uri->segment(1) . '/' . $CI->uri->segment(2);
 }
 
-function CalculaRecargo($IDExp) {
+function CalculaRecargo($IDExp, $IDUsuario) {
     $hoy = date('Y-m-d');
     $CI = & get_instance();
+    $recargo = 0;
 
-//    $sql = "select * from parametrosgenerales where Tipo = '" . $Tipo . "'";
-//    $query = $CI->db->query($sql);
-//
-//    if ($query->num_rows() > 0) {
-//        $array = $query->result_array();
-//    } else {
-//        $array = array();
-//    }
+    $sql = "select * from serviciocontratado as sc
+            left join tiene_recargo as tr on tr.Servicio_IDServicio = sc.Servicio_IDServicio
+            left join pago_has_servicio as ps on ps.Expediente_IDExp = sc.Expediente_IDExp and ps.Servicio_IDServicio = tr.Servicio_IDServicio
+            where sc.Expediente_IDExp = " . $IDExp . " and sc.Usuario_IDUsuario = " . $IDUsuario;
+    $query = $CI->db->query($sql);
+
+    if ($query->num_rows() > 0) {
+        $array = $query->result_array();
+        $recargo = $array[0]["Penalizacion"];
+    } else {
+        $array = array();
+    }
+    return $recargo;
 }
 
 function getDocentesAsignadosMateria($IDMateria) {
@@ -296,7 +302,7 @@ function getDocentesAsignadosMateria($IDMateria) {
     return $array;
 }
 
-function getNombreDocumentoExp($IDExp,$IDTipoDoc) {
+function getNombreDocumentoExp($IDExp, $IDTipoDoc) {
     $CI = & get_instance();
 
     $NomDoc = "";
@@ -315,7 +321,8 @@ function getNombreDocumentoExp($IDExp,$IDTipoDoc) {
 /* * **************************************************************************
  * FUNCIONES GENERALES DE VALORES FIJOS			            			     *
  * ************************************************************************** */
-function DiasSemana(){
+
+function DiasSemana() {
     $data = array(
         "Lun" => "Lunes",
         "Mar" => "Martes",
@@ -325,13 +332,25 @@ function DiasSemana(){
     return $data;
 }
 
-function Meses(){
-    $data = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto",
-        "Septiembre","Octubre","Noviembre","Diciembre");
+function NumDiasSemana() {
+    $data = array(
+        1 => "Lunes",
+        2 => "Martes",
+        3 => "Miercoles",
+        4 => "Jueves",
+        5 => "Viernes",
+        6 => "Sabado",
+        7 => "Domingo");
     return $data;
 }
 
-function PeriodosPago(){
+function Meses() {
+    $data = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+        "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    return $data;
+}
+
+function PeriodosPago() {
     $data = array(
         "SEMANAL" => "SEMANAL",
         "QUINCENAL" => "QUINCENAL",
@@ -339,6 +358,70 @@ function PeriodosPago(){
     return $data;
 }
 
+function GeneraFechaPago($FrecPago, $Dia) {
+    $hoy = date('Y-m-d');
+    $fecha_pago = null;
+    $days = array(
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+        6 => "Saturday",
+        7 => "Sunday");
+    $dias = NumDiasSemana();
+
+    switch ($FrecPago) {
+        case "SEMANAL":
+            $date = new DateTime($hoy);
+            if ($date->format('N') != $Dia) {
+                $date->modify('Next ' . $days[$Dia]);
+            }
+            $fecha_pago = 'Sem. ' . $dias[$Dia] . ', ' . $date->format('d/m/Y');
+            break;
+        case "QUINCENAL":
+            $dia = date('d');
+            $tmp = date('Y-m');
+            $numdias = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
+
+            if ($dia <= 15) {
+                $date = new DateTime($tmp . '-15');
+                $fecha_pago = 'Quincenal, ' . $date->format('d/m/Y');
+            } else {
+                $quina = $numdias == 30 ? $numdias == 29 ? 92 : 30 : 31;
+                $date = new DateTime($tmp . '-' . $quina);
+                $fecha_pago = 'Quincenal, ' . $date->format('d/m/Y');
+            }
+            break;
+        case "MENSUAL":
+            $tmp = date('Y-m') . '-' . str_pad($Dia, 2, "0", STR_PAD_LEFT);
+            $date = new DateTime($tmp);
+            $date->modify('Next Month');
+            $fecha_pago = 'Mensual, ' . $date->format('d/m/Y');
+            break;
+
+        default:
+            break;
+    }
+
+    return $fecha_pago;
+}
+
+function FechaUltimoPago($IDExp) {
+    $CI = & get_instance();
+
+    $fecha = "";
+    $sql = "select FecPago from pago where Expediente_IDExp = " . $IDExp . " order by FecPago desc";
+    $query = $CI->db->query($sql);
+
+    if ($query->num_rows() > 0) {
+        $array = $query->result_array();
+        $fecha = $array[0]['FecPago'];
+    } else {
+        $fecha = "1901-01-01 00:00:01";
+    }
+    return $fecha;
+}
 
 /* * **************************************************************************
  * FUNCIONES GENERALES PARA USO DE DBs			            			     *
