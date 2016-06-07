1212c32ee6b2d1713bf2ec8d9517fb548394849b
esuarez <esuarez@carloslap.rocasis.com>
2016-06-07 13:40:40 -0500

    cambio en la vista general

--- application/views/example.php
+++ application/views/example.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php 
-foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-<?php endforeach; ?>
-<?php foreach($js_files as $file): ?>
-	<script src="<?php echo $file; ?>"></script>
-<?php endforeach; ?>
-<style type='text/css'>
-body
-{
-	font-family: Arial;
-	font-size: 14px;
-}
-a {
-    color: blue;
-    text-decoration: none;
-    font-size: 14px;
-}
-a:hover
-{
-	text-decoration: underline;
-}
-</style>
-</head>
-<body>
-<div id="content">
-    <div class="container-fluid">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php foreach ($css_files as $file): ?>
+            <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+        <?php endforeach; ?>
+        <?php foreach ($js_files as $file): ?>
+            <script src="<?php echo $file; ?>"></script>
+        <?php endforeach; ?>
+        <style type='text/css'>
+            body
+            {
+                font-family: Arial;
+                font-size: 14px;
+            }
+            a {
+                color: blue;
+                text-decoration: none;
+                font-size: 14px;
+            }
+            a:hover
+            {
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="content">
+            <div class="container-fluid">
 
-    <div class="panel panel-default">
-		<?php echo $output; ?>
-    </div>
-	</div>
-	</div>
-</body>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="media">
+                            <div class="media-body message">
+                                <div class="panel panel-default">
+                                    <?php echo $output; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
