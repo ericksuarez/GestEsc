0d2d9c50eb191d645b7dc518d5bd65cbf4b459c1
esuarez <esuarez@carloslap.rocasis.com>
2016-06-07 13:42:53 -0500

    se agregaron las rutas FTP de la ubicacion del upload de tareas y notas

--- application/config/config_controlEscolar.php
+++ application/config/config_controlEscolar.php
@@ -8,4 +8,7 @@ $config['Direccion'] = 'Bondojito 238, 01520 D.F. 01 55 5277 2460';
 
 //RUTA PARA GUARDAR LOS DOCUMENTOS QUE SUBEN LOS USUARIO (la ruta siempre debe de empezar con ./)
 $config['RutaFTP'] = "./assets/uploads/files/";
-
+//RUTA PARA SUBIR LAS NOTAS DE LAS MATERIAS
+$config['RutaNota'] = "assets/uploads/files/Notas";
+//RUTA PARA SUBIR LAS TAREAS POR PARTE DE LOS DOCENTES Y ESTUDIANTES
+$config['RutaTarea'] = "assets/uploads/files/Tareas";
