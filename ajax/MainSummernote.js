54bf0e597cdfbed0ec2a4691d8dbf0cb68004c1c
esuarez <esuarez@carloslap.rocasis.com>
2016-06-14 12:26:25 -0500

    se usa para cargar el CKEditor cuando se va enviar a los destinatarios

---
+++ ajax/MainSummernote.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+    var URL = 'http://localhost:8080/controlEscolar/index.php/';
+
+// Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor');
+
+//    $('.summernote').summernote({
+//        height: 300,
+//        tabsize: 2
+//    });
+
+
+});
+
