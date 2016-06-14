e9cc2d5b5474403edf5137f4037602d4c7cf598c
esuarez <esuarez@carloslap.rocasis.com>
2016-06-14 12:41:36 -0500

    se cambiaron para poder usar ckfinder y subir imagenes al servidor

--- assets/grocery_crud/texteditor/ckeditor/config.js
+++ assets/grocery_crud/texteditor/ckeditor/config.js
@@ -1,11 +1,19 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.skin = 'bootstrapck';
+	// added code for ckfinder ------>
+	 config.filebrowserBrowseUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/ckfinder.html';
+	 config.filebrowserImageBrowseUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/ckfinder.html?type=Images';
+	 config.filebrowserFlashBrowseUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/ckfinder.html?type=Flash';
+	 config.filebrowserUploadUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/core/connector /php/connector.php?command=QuickUpload&type=Files';
+	 config.filebrowserImageUploadUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+	 config.filebrowserFlashUploadUrl = '/grocery/assets/grocery_crud/texteditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+// end: code for ckfinder ------>
 };
