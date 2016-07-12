c66b45cf2c0cd9f86e5ab6a0de3adadd21e79211
esuarez <esuarez@carloslap.rocasis.com>
2016-07-12 17:03:49 -0500

    se creo el calendario escolar esta visible para todos los usuarios

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,13 +1,7 @@
-<!-- content push wrapper -->
-<div class="st-pusher">
-    <!-- this is the wrapper for the content -->
-    <div class="st-content">
-        <!-- extra div for emulating position:fixed of the menu -->
-        <div class="st-content-inner padding-top-none" id="content" style="padding-left: 20px; padding-right: 20px;">
-            <?php echo $catalogo["tipo_usuario"];?>
-        </div>
-        <!-- /st-content-inner -->
+<div id="content">
+    <div class="container-fluid">
+        <iframe src="<?php echo site_url('calendar/agenda') ?>" 
+                width="100%" height="800px" scrolling="yes" frameBorder="0">
+        </iframe> 
     </div>
-    <!-- /st-content -->
 </div>
-<!-- /st-pusher -->
