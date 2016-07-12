27da1d971120853909af9f614d29b29bb19c06e1
esuarez <esuarez@carloslap.rocasis.com>
2016-07-12 17:02:51 -0500

    se creo el calendario escolar esta visible para todos los usuarios

---
+++ application/controllers/Calendar.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Calendar extends CI_Controller {
+
+    public function index() {
+        $this->load->view('common/header');
+        $this->load->view('home_view');
+        $this->load->view('common/footer');
+    }
+
+    public function agenda() {
+        $this->load->view('calendar/calendar');
+    }
+
+}
