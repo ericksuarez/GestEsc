f0961a90a3b1999c48a90b612f34e77ab880fb7b
esuarez <esuarez@carloslap.rocasis.com>
2016-08-14 18:40:47 -0500

    Se creo la fucnionalidad para la creacion de la recuperacion del password

---
+++ application/views/recuperacion_password.php
@@ -0,0 +1,46 @@
+<div class="form-input-box" id="Tema_input_box">
+    <div id="field-Tema" class="readonly_label"><p>&nbsp;</p>
+        <table align="center" border="0" cellpadding="1" cellspacing="0">
+            <tbody>
+                <tr>
+                    <td style="width:423px">
+                        <p><img src="<?php echo base_url() . 'images/logo.jpg' ?>" style="float:left; height:108px; width:108px">
+                            <img src="<?php echo base_url() . 'images/recuperacion.png' ?>" style="float:right; height:52px; width:52px"></p>
+                        <p style="text-align:center"><strong>Solicitud de recuperación de cuenta.</strong></p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width:423px">
+                        <p>&nbsp;</p>
+                        <p><strong>Buenas tardes&nbsp;</strong></p>
+                        <p><br>
+                            <strong>Por medio de la presente se le informa que se ha solicitado una recuperación de la contraseña de este correo para el acceso al sistema escolar</strong><strong>.</strong></p>
+                        <p>&nbsp;</p>
+                        <p><em>Correo:</em><?php echo $correo ?></p>
+                        <p><em>Usuario:</em><?php echo $usuario ?></p>
+                        <p><em>Contraseña:</em><?php echo $contrasena ?></p>
+                        <p>&nbsp;</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width:423px">
+                        <p>&nbsp;</p>
+                        <p><strong><em>¿No reconoces esta actividad?</em></strong><br>
+                            <em><strong>Contacta a un administrador.</strong></em></p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width:423px">
+                        <p><strong>Te hemos enviado este correo electrónico de anuncio de servicio obligatorio para informarte sobre una serie de cambios importantes que afectan a tu cuenta.</strong></p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div style="clear:both;">&nbsp;</div>
+        <p>&nbsp;</p>
+    </div>
+</div>
